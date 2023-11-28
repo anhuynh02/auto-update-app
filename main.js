0190f19174e2cd9b71fda7e9ca81e2fade466dda
S52000719 <105099290+anhuynh02@users.noreply.github.com>
2023-11-28 15:45:52 +0700

    add auto update

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
-const { app, BrowserWindow, ipcMain } = require('electron')
-const path = require('node:path')
+const { app,BrowserWindow , ipcMain, autoUpdater, dialog } = require('electron')
+const path = require("path")
+const server = 'https://auto-update-app-agxu.vercel.app'
+const url = `${server}/update/${process.platform}/${app.getVersion()}`
 
-function createWindow () {
+
+function createWindow() {
   const mainWindow = new BrowserWindow({
     webPreferences: {
-      
+
       preload: path.join(__dirname, 'preload.js')
     }
   })
@@ -20,7 +23,28 @@ function createWindow () {
 
 app.whenReady().then(() => {
   createWindow()
+  autoUpdater.setFeedURL({ url })
+  setInterval(() => {
+    autoUpdater.checkForUpdates()
+  }, 60000)
+  autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
+    const dialogOpts = {
+      type: 'info',
+      buttons: ['Restart', 'Later'],
+      title: 'Application Update',
+      message: process.platform === 'win32' ? releaseNotes : releaseName,
+      detail:
+        'A new version has been downloaded. Restart the application to apply the updates.'
+    }
 
+    dialog.showMessageBox(dialogOpts).then((returnValue) => {
+      if (returnValue.response === 0) autoUpdater.quitAndInstall()
+    })
+  })
+  autoUpdater.on('error', (message) => {
+    console.error('There was a problem updating the application')
+    console.error(message)
+  })
   app.on('activate', function () {
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
