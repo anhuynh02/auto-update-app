0f98f21e9c2209688028a434333683e344095894
S52000719 <105099290+anhuynh02@users.noreply.github.com>
2023-11-28 15:58:17 +0700

    updata module

--- main.js
+++ main.js
@@ -21,35 +21,47 @@ function createWindow() {
   mainWindow.loadFile('index.html')
 }
 
+// Set up auto-updater
 app.whenReady().then(() => {
-  createWindow()
-  autoUpdater.setFeedURL({ url })
+  createWindow();
+  console.log(app.app.getVersion())
+  // Set the feed URL for auto-updater
+  autoUpdater.setFeedURL(url);
+
+  // Check for updates every 10 seconds
   setInterval(() => {
-    autoUpdater.checkForUpdates()
-  }, 60000)
+    autoUpdater.checkForUpdates();
+  }, 10000);
+
+  // Event: Update downloaded
   autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
+    // Display a dialog to prompt the user to restart and apply updates
     const dialogOpts = {
       type: 'info',
       buttons: ['Restart', 'Later'],
       title: 'Application Update',
       message: process.platform === 'win32' ? releaseNotes : releaseName,
-      detail:
-        'A new version has been downloaded. Restart the application to apply the updates.'
-    }
+      detail: 'A new version has been downloaded. Restart the application to apply the updates.'
+    };
 
     dialog.showMessageBox(dialogOpts).then((returnValue) => {
-      if (returnValue.response === 0) autoUpdater.quitAndInstall()
-    })
-  })
-  autoUpdater.on('error', (message) => {
-    console.error('There was a problem updating the application')
-    console.error(message)
-  })
-  app.on('activate', function () {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
+      if (returnValue.response === 0) autoUpdater.quitAndInstall();
+    });
+  });
 
+  // Event: Error in auto-updater
+  autoUpdater.on('error', (error) => {
+    console.error('Error updating the application:', error);
+  });
+
+  // Handle window activation
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+// Handle window closure
 app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
-})
+  if (process.platform !== 'darwin') app.quit();
+});
+
